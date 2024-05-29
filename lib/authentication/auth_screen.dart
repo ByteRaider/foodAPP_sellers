d33e3f4a8b775915fd382cc9aec6c750f1dd905d
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-29 13:17:10 -0700

    Add TabBarView to auth screen

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -10,10 +10,55 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Auth Screen"),
-      ),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.deepPurple.shade300],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 0.0),
+                      stops: const [0.0, 1.0],
+                      tileMode: TileMode.clamp)),
+            ),
+            title: const Text(
+              "Vende tu comida en linea",
+              style: TextStyle(fontFamily: "Lobster"),
+            ),
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  text: "Login",
+                  icon: Icon(Icons.lock),
+                ),
+                Tab(
+                  text: "Register",
+                  icon: Icon(Icons.person_add),
+                ),
+              ],
+              indicatorColor: Colors.white38,
+              indicatorWeight: 5,
+            ),
+          ),
+          body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            )),
+            child: const TabBarView(
+              children: [
+                Text("Login"),
+                Text("Register"),
+              ],
+            ),
+          ),
+        ));
   }
 }
