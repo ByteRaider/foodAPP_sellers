380d85480de46104ff92e74ff1dd053140fc2fd7
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-28 17:56:21 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mexa_sellers_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mexa_sellers_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String text = 'Sellers App';
+
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: text,
+      // theme: AppTheme.lightTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(text),
+        ),
+      ),
+    );
+  }
+}
