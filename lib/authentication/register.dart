3e1537d6acec89ddf57b731ea56db2d729ef42a4
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-31 17:12:42 -0700

    Add circle avatar to regiter screen

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mexa_sellers_app/theme/app_theme.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,8 +12,38 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController anyController = TextEditingController();
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(2),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+            InkWell(
+              child: CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.20,
+                backgroundColor: AppTheme.primary,
+                backgroundImage: imageXFile == null
+                    ? null
+                    : FileImage(File(imageXFile!.path)),
+                child: imageXFile == null
+                    ? Icon(
+                        Icons.add_photo_alternate,
+                        color: Colors.white,
+                        size: MediaQuery.of(context).size.width * 0.20,
+                      )
+                    : null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const Color primary = Colors.green;
+  static const Color secondary = Colors.deepPurpleAccent;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     appBarTheme: const AppBarTheme(backgroundColor: primary, elevation: 0),
     scaffoldBackgroundColor: Colors.white,
