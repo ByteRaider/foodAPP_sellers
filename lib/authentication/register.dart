d2637ff8d9f2547f2540395ed8adbf6fe2370ee4
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-05-31 17:25:12 -0700

    Create register CustomField controllers

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mexa_sellers_app/theme/app_theme.dart';
 
+import '../widgets/custom_text_field.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
@@ -12,7 +14,13 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  TextEditingController anyController = TextEditingController();
+  final _globalKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
 
@@ -20,10 +28,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        padding: const EdgeInsets.all(2),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
+          padding: const EdgeInsets.all(2),
+          child: Column(mainAxisSize: MainAxisSize.max, children: [
             SizedBox(height: MediaQuery.of(context).size.height * 0.1),
             InkWell(
               child: CircleAvatar(
@@ -41,9 +47,53 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     : null,
               ),
             ),
-          ],
-        ),
-      ),
+            const SizedBox(
+              height: 10,
+            ),
+            Form(
+              key: _globalKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    controller: nameController,
+                    data: Icons.person,
+                    hintText: "Name",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    controller: phoneController,
+                    data: Icons.phone,
+                    hintText: "Phone",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    controller: passwordController,
+                    data: Icons.lock,
+                    hintText: "Password",
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    controller: confirmPasswordController,
+                    data: Icons.lock,
+                    hintText: "Confirm Password",
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    controller: emailController,
+                    data: Icons.email,
+                    hintText: "Email",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    controller: locationController,
+                    data: Icons.location_pin,
+                    hintText: "Location",
+                    isObscure: false,
+                  ),
+                ],
+              ),
+            ),
+          ])),
     );
   }
 }
